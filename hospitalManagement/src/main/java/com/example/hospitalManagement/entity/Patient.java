d746d472dcf3c7eb55423ae1848c5f3d0d5e3f32
sauravknt <sauravknt@gmail.com>
2026-05-19 16:12:50 +0530

    first commit hospital management 1

---
+++ hospitalManagement/src/main/java/com/example/hospitalManagement/entity/Patient.java
@@ -0,0 +1,25 @@
+package com.example.hospitalManagement.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDate birthDate;
+
+    private String email;
+
+    private String gender;
+}
