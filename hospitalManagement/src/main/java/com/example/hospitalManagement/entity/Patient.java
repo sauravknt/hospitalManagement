e321399ce713b553fbb844b696fbc3efea984ef4
sauravknt <sauravknt@gmail.com>
2026-06-28 01:10:17 +0530

    second commit hospital management 1

--- hospitalManagement/src/main/java/com/example/hospitalManagement/entity/Patient.java
+++ hospitalManagement/src/main/java/com/example/hospitalManagement/entity/Patient.java
@@ -1,25 +1,55 @@
 package com.example.hospitalManagement.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.example.hospitalManagement.entity.type.BloodGroupType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+//@RequiredArgsConstructor
+@Getter
+@Setter
+@Table(
+        name="patient",
+       uniqueConstraints = {
+                @UniqueConstraint(name="1unique_patient_email",columnNames = {"email"}),
+               @UniqueConstraint(name="1unique_patient_name_birthdate",columnNames = {"name","birth_date"})
+       },
+    indexes = {
+              @Index(name="idx_patient_birth_date1",columnList = "birthDate")
+        }
+)
 public class Patient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false,length = 40)
     private String name;
 
+  //  @ToString.Exclude
+    @Column(name="birth_date")
     private LocalDate birthDate;
 
+    @Column(unique = true,nullable = false)
     private String email;
 
     private String gender;
-}
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @Enumerated(EnumType.STRING)
+    private BloodGroupType bloodGroup;
+
+
+    }
+
