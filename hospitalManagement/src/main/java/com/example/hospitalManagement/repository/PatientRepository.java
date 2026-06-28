b3b33776a57b8b98398c7ea02c75f9aebcb2f211
sauravknt <sauravknt@gmail.com>
2026-06-29 03:00:22 +0530

    commit native query and JPQL

--- hospitalManagement/src/main/java/com/example/hospitalManagement/repository/PatientRepository.java
+++ hospitalManagement/src/main/java/com/example/hospitalManagement/repository/PatientRepository.java
@@ -1,7 +1,12 @@
 package com.example.hospitalManagement.repository;
 
 import com.example.hospitalManagement.entity.Patient;
+import com.example.hospitalManagement.entity.type.BloodGroupType;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -15,4 +20,26 @@ public interface PatientRepository extends JpaRepository<Patient,Long> {
     List<Patient> findByBirthDateOrEmail(LocalDate birthDate, String email);
 
     List<Patient> findByNameContaining(String di);
+
+    @Query("SELECT p FROM Patient p WHERE p.bloodGroup=?1")
+    List<Patient> findByBloodGroup(@Param("bloodGroup") BloodGroupType bloodGroup);
+
+    @Query("select p from Patient p where p.birthDate > :birthDate")
+    List<Patient> findByBornAfterDate(@Param("birthDate")LocalDate birthDate);
+
+    @Query("select p.bloodGroup, Count(p) from Patient p group by p.bloodGroup")
+   // List<Object[]> countEachBloodGroupType(@Param("bloodGroup") BloodGroupType bloodGroupType);
+    List<Object[]> countEachBloodGroupType();
+
+    //native query
+    @Query(value = "select * from patient", nativeQuery = true)
+    List<Patient> findAllPatients();
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Patient p SET p.name= :name where p.id=:id")
+    int updateNameWithId(@Param("name") String name,@Param("id") Long id);
+
+
+
 }
--- hospitalManagement/src/test/java/com/example/hospitalManagement/PatientTest.java
+++ hospitalManagement/src/test/java/com/example/hospitalManagement/PatientTest.java
@@ -1,6 +1,7 @@
 package com.example.hospitalManagement;
 
 import com.example.hospitalManagement.entity.Patient;
+import com.example.hospitalManagement.entity.type.BloodGroupType;
 import com.example.hospitalManagement.repository.PatientRepository;
 import com.example.hospitalManagement.service.PatientService;
 import org.junit.jupiter.api.Test;
@@ -32,7 +33,8 @@ public class PatientTest {
         p1.setEmail("test@gmail.com");
         patientRepository.save(p1); */
        // List<Patient> patientList=patientRepository.findByBirthDateOrEmail(LocalDate.of(1988,3,15),"diya.patel@example.com");
-        List<Patient> patientList=patientRepository.findByNameContaining("Di");
+      //  List<Patient> patientList=patientRepository.findByNameContaining("Di");
+        List<Patient> patientList=patientRepository.findByBloodGroup(BloodGroupType.A_POSITIVE);
         for(Patient patient:patientList)
         {
             System.out.println(patient);
@@ -40,12 +42,27 @@ public class PatientTest {
     }
 
     @Test
-    public void testTransactionMethods()
-    {
-      // Patient patient=patientService.getPatientById(1L);
-      // System.out.println(patient);
-        Patient patient=patientRepository.findByName("Diya Patel");
-       // Patient patient1=patientRepository.findByBirthDate("sa");
-        System.out.println(patient);
+    public void testTransactionMethods() {
+        // Patient patient=patientService.getPatientById(1L);
+        // System.out.println(patient);
+        //   Patient patient=patientRepository.findByName("Diya Patel");
+        // Patient patient1=patientRepository.findByBirthDate("sa");
+        //  System.out.println(patient);
+      //  List<Patient> patientList = patientRepository.findByBornAfterDate(LocalDate.of(1993, 3, 14));
+      /*  List<Patient> patientList=patientRepository.findAllPatients();
+        for (Patient patient : patientList) {
+            System.out.println(patient);
+        }
+
+        List<Object[]> bloodGroupList = patientRepository.countEachBloodGroupType();
+        for (Object[] objects : bloodGroupList) {
+            System.out.println(objects[0] + " " + objects[1]);
+        } */
+
+        int rowsUpdated=patientRepository.updateNameWithId("Arab Sharma",1L);
+        System.out.println(rowsUpdated);
+
+
     }
-}
+    }
+
